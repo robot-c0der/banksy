f3751a3cad917b2c4de9f760a627d01262d8bb29
Ashe <ashe@adamwk.com>
2023-08-13 19:15:10 -0700

    User Attributes
    - Add has_secure_password
    - Allow attribute access
    - Add uniqueness validation for email and login_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    attr_accessible :email, :password, :password_confirmation, :login_name
+
+    validates_uniqueness_of :email, :login_name
 end
