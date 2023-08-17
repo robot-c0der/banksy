a0d599368f5dddb57b4a5c6ac2e9ad039ac95e0d
Ashe <ashe@adamwk.com>
2023-08-16 23:10:14 -0700

    Update User model
    - Add confirmation token methods
    - Add has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
+    CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
+    has_secure_password
+
     validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, unique: true
     validates_presence_of :name
+
+    def confirm!
+        update_columns(confirmed_at: Time.current)
+    end
+
+    def confirmed?
+        confirmed_at.present?
+    end
+
+    def generate_confirmation_token
+        signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: confirm_email
+    end
+    
+    def unconfirmed?
+        !confirmed?
+    end
 end
