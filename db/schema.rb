6f84e328eddfc598bc31143d6fdd184dbb6e4166
Ashe <ashe@adamwk.com>
2023-08-16 23:01:59 -0700

    migrate DB to create User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_055623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "uniq_emails", unique: true
+  end
 
 end
