9072d2b930c8ab5fba2fe20b4097549279e992ab
Ashe <ashe@adamwk.com>
2023-08-16 22:51:36 -0700

    Restart the Users Stuff
    - removing users stuff so we can restart it

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-
-    attr_accessible :email, :password, :password_confirmation, :login_name
-
-    validates_uniqueness_of :email, :login_name
-end
