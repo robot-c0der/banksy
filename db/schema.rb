e4e5502292f4d74cf23ca60136ee8dac3fd7eac2
Ashe <ashe@adamwk.com>
2023-08-21 16:28:15 -0700

    add unconfirmed email column to User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_060411) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_232330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_060411) do
     t.datetime "updated_at", null: false
     t.datetime "confirmed_at"
     t.string "password_digest", null: false
+    t.string "uncomfirmed_email"
     t.index ["email"], name: "uniq_emails", unique: true
   end
 
