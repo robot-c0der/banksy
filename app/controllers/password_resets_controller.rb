85943880cf47301aa509929b750187ae24dc0d8c
Ashe <ashe@adamwk.com>
2023-08-21 15:49:30 -0700

    Password Resets
    - Create controller for Password Resets
    - Create email templates for password resets
    - Add password reset forms
    - Add password reset routes

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,54 @@
+class PasswordResetsController < ApplicationController
+    before_action :redirect_if_authenicated
+
+    def create
+        @user = User.find_by(email: params[:user][:email].downcase)
+        if @user.present?
+            if @user.confirmed?
+                @user.send_confirmation_email!
+                redirect_to login_path, notice: "Password reset email sent"
+            else
+                redirect_to new_confirmation_path, alert: "Please confirm your email first"
+            end
+        else
+            redirect_to login_path, notice: "Password reset email sent"
+        end
+    end
+
+    def edit
+        @user = User.find_signed(params[:pasword_reset_token], purpose: :reset_password)
+
+        if @user.present? && @user.unconfirmed?
+            redirect_to new_confirmation_path, alert: "You must confirm your email before you can sign in"
+        elsif @user.nil?
+            redirect_to new_password_reset_path, alert: "Invalid or expired token"
+        end
+    end
+
+    def new
+    end
+
+    def update
+        @user = User.find_signed(params[:password_reset_token], purpose: :reset_password)
+
+        if @user
+            if @user.unconfirmed?
+                redirect_to new_confirmation_path, alert: "You must confirm your email before you can sign in"
+            elsif @user.update(password_params)
+                redirect_to login_path, notice: "Password updated"
+            else
+                flash.now[:alert] = @user.errors.full_messages.to_sentence
+                render :edit, status: :unprocessable_entity
+            end
+        else
+            flash.now[:alert] = "Invalid or Expired token"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   get "login", to: "sessions#new"
+
+  resources :password_resets, only: [:create, :edit, :new, :update], param: :pasword_reset_token
 end
