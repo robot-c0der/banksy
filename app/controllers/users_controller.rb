bdaceb2f9a0b2c123259aa682b08fabff8c03aa3
Ashe <ashe@adamwk.com>
2023-08-21 17:43:01 -0700

    Account Update Form
    - Add edit.html.erb for editing user details
    - Update UsersController to have edit and update handling (also
    account deletion but that route is commented out for now)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!, only: [:edit, :destroy, :update]
     before_action :redirect_if_authenicated, only: [:create, :new]
     
     def create
-        @user = User.new(user_params)
+        @user = User.new(create_user_params)
         if @user.save
             @user.send_confirmation_email!
             redirect_to root_path, notice: "Please check your email for confirmation instructions"
@@ -15,10 +16,46 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def destroy
+        current_user.destroy
+        reset_session
+        redirect_to root_path, notice: "Account deleted"
+    end
+
+    def edit
+        @user = current_user
+    end
+
+    def update
+        @user = current_user
+
+        if @user.authenticate(params[:user][:current_password])
+            if @user.update(update_user_params)
+                if params[:unconfirmed_email].present?
+                    @user.send_confirmation_email!
+                    flash.now[:notice] = "Check your new email address for confirmation instructions"
+                    render :edit
+                else
+                    flash.now[:notice] = "Account updated"
+                    render :edit
+                end
+            else
+                render :edit, status: :unprocessable_entity
+            end
+        else
+            flash.now[:error] = "Incorrect password"
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     private
 
-    def user_params
+    def create_user_params
         params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+
+    def update_user_params
+        params.require(:user).permit(:current_password, :password, :password_confirmation, :unconfirmed_email)
     end
 
 end
