885cafbcbfb9dfb1d601b7019963ceac5a18e29c
Ashe <ashe@adamwk.com>
2023-09-17 07:37:55 +0000

    Add SMTP Configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "banksy.ioerror.ca" }
-  # config.action_mailer.smtp_settings = { port: 2525 }
+  config.action_mailer.default_url_options = { host: ENV['PUBLIC_BASE_URL'] }
+  config.action_mailer.smtp_settings = { 
+    address: ENV['SMTP_HOST'],
+    user_name: ENV['SMTP_USER'],
+    password: ENV['SMTP_PASSWORD'],
+    port: ENV['SMTP_PORT'],
+    tls: ENV['SMTP_TLS'],
+    ssl: ENV['SMTP_SSL']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
