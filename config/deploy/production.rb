057d82874c6fa4059056f69829f5ddebfda992cb
Ashe <ashe@adamwk.com>
2023-09-26 20:18:35 -0700

    fixed a couple more things that should be environment variables

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '159.203.14.4', user: 'autodep', roles: %w{app db web}
+server "#{ENV['CAP_DEPLOY_SERVER_IP']}", user: "#{ENV['CAP_DEPLOY_USER']}", roles: %w{app db web}
 
 
  set :ssh_options, {
